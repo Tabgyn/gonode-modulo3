df59d8daa52f98ebd00f73acbbf3887535b25ab7
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-04-25 18:56:14 -0300

    CRUD de Ad

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 const express = require('express')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const controllers = require('./app/controllers')
+const authMiddleware = require('./app/middlewares/auth')
 
 const routes = express.Router()
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
+
+routes.use(authMiddleware)
+
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
